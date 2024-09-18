5fc018bc2037446e5541604c21c989b4f73e455a
Satorugg <158627276+Satorugg@users.noreply.github.com>
2024-09-18 17:44:25 +0000

    setup

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Battle Simulator</h1>
+      <h2>Current Troops:</h2>
+    </div>
+  );
+}
+
+export default App;
