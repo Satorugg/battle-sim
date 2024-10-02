27add53deb91783784f4e668c9d396a6bc6db415
Satar Hassni <Satar.Hassni@liu.edu>
2024-10-02 12:04:10 -0400

    fr fixed

--- api/src/main/java/com/battlesim/api/ApiApplication.java
+++ api/src/main/java/com/battlesim/api/ApiApplication.java
@@ -2,9 +2,17 @@ package com.battlesim.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ApiApplication {
+
+	@GetMapping("/hello")
+		public String check() {
+		return "Hello World";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
